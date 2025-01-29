b8ae051e5e56d40862162b00baa6c3be54994f21
Danyss09 <danielacaceresp4@gmail.com>
2025-01-29 13:53:41 -0500

    Update reservation_controller.py

--- controllers/reservation_controller.py
+++ controllers/reservation_controller.py
@@ -3,14 +3,11 @@ from models.reservation_model import delete_reservation_by_id
 
 reservation_bp = Blueprint('reservation', __name__)
 
-# Ruta para eliminar una reserva
 @reservation_bp.route('/delete_reservation/<int:reservation_id>', methods=['DELETE'])
 def delete_reservation_route(reservation_id):
     response = delete_reservation_by_id(reservation_id)
-    
-    # Si la reserva no se encuentra o hay un error, devolvemos el error
+
     if "error" in response:
         return jsonify(response), 404
-    
-    # Si la eliminación es exitosa, devolvemos el mensaje de éxito
+
     return jsonify(response), 200
